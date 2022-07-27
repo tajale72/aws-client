1e5c98ab5f0076691b719c02f91426168411fcbf
Tajale72 <Tajale72@gmail.com>
2022-07-26 22:44:33 -0500

    added backgrounf in signup

---
+++ aws-client/src/component/Register/Register.jsx
@@ -0,0 +1,10 @@
+
+function Register() {
+    return (
+        <div>
+            This is Register page
+        </div>
+    )
+}
+
+export default Register
--- aws-client/src/component/Signup/Signup.jsx
+++ aws-client/src/component/Signup/Signup.jsx
@@ -1,10 +1,16 @@
 import Navbar from '../Navbar/Navbar'
+import './Signup.css';
+//import Register from '../Register/Register'
 
 function Signup() {
     return (
-        <div>
-             <Navbar />
-            This is Signup page
+        <div className="signup">
+        <Navbar /> 
+        <div className="register-title"><h1>Register</h1></div>
+        <div className="registration">
+        <h1>This is a signup page</h1>
+        </div> 
+       
         </div>
     )
 }
