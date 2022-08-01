f82ee835faf703bb65b7e01ca02f48dba52cde8b
Tajale72 <Tajale72@gmail.com>
2022-07-31 20:51:59 -0500

    added video

--- aws-client/src/component/Home/Home.jsx
+++ aws-client/src/component/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Navbar from '../Navbar/Navbar'
 import './Home.css';
 import bakery_home from '../images/bakery_home.jpeg';
+import React from 'react';
+import ReactPlayer from 'react-player'
+
 
 function Home() {
     return (
         <div className="home">
              <Navbar />
+             <div className="video-player">
+             <ReactPlayer width='100%' url='https://www.youtube.com/watch?v=XNXuwrXFHRs' />
+             </div>
              <img src={bakery_home} alt="name" className="img-car"/>
         </div>
     )
